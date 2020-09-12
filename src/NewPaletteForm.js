2e5287226e5f22191600e3bbfc03e7a7c13fca60
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-13 01:09:20 +0530

    implement Drag and Drop

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -15,8 +15,9 @@ import Button from "@material-ui/core/Button"
 
 import { ChromePicker } from 'react-color'
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import {arrayMove} from 'react-sortable-hoc'
 
-import DraggableColorBox from './DraggableColorBox'
+import DraggableColorList from './DraggableColorList'
 
 const drawerWidth = 400;
 
@@ -150,6 +151,12 @@ export class NewPaletteForm extends Component {
     })
   }
 
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState(({colors}) => ({
+      colors: arrayMove(colors, oldIndex, newIndex),
+    }));
+  };
+
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -247,14 +254,12 @@ export class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-          {this.state.colors.map(color=>(
-            <DraggableColorBox
-              key={color.name}
-              color={color.color}
-              name={color.name}
-              handleClick={()=>this.removeColor(color.name)}
-            />
-          ))}
+          <DraggableColorList
+            colors={this.state.colors}
+            removeColor={this.removeColor}
+            axis="xy"
+            onSortEnd={this.onSortEnd}
+          />
         </main>
       </div>
     );
