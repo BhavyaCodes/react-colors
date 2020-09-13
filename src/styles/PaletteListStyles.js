8f628e70d6779adbd9a3a0a70bb62f3a0f97aa6d
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-14 01:34:41 +0530

    add bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,18 @@
 import sizes from './sizes'
+import bg from './bg.svg'
 
 export default {
 	root: {
-		backgroundColor: "blue",
 		height: "100vh",
 		display: "flex",
 		alignItems: "flex-start",
-		justifyContent: "center"
+		justifyContent: "center",
+		backgroundColor: "#1119aa",
+		backgroundImage: `url(${bg})`,
+		overflowY: "scroll"
+	},
+	heading:{
+		fontSize: "2rem"
 	},
 	container:{
 		width: "50%",
